615582105634c7283eebb05c5c626b184e67b7e2
Julien Loir <6706489+Namaneo@users.noreply.github.com>
2022-10-13 12:44:15 +0200

    Update webview examples

--- examples/3-webview.c
+++ examples/3-webview.c
@@ -6,7 +6,6 @@
 struct context {
 	MTY_App *app;
 	MTY_Window window;
-	MTY_Webview *webview;
 	bool quit;
 };
 
@@ -29,26 +28,6 @@ static bool app_func(void *opaque)
 	return !ctx->quit;
 }
 
-static void on_webview_created(MTY_Webview *webview, void *opaque)
-{
-	MTY_WebviewEnableDevTools(webview, true);
-
-	MTY_WebviewAddResource(webview, "style.css", "body { background-color: darkslategrey; }", 0);
-	MTY_WebviewAddResource(webview, "script.js", "document.getElementById('my_id').style.color = 'goldenrod';", 0);
-	
-	MTY_WebviewNavigateHTML(webview, 
-		"<html>"
-		"  <head>"
-		"    <link rel='stylesheet' href='mty://app.local/style.css'>"
-		"  </head>"
-		"  <body>"
-		"    <span id='my_id'>Hello World!</span>"
-		"    <script src='mty://app.local/script.js'></script>"
-		"  </body>"
-		"</html>"
-	);
-}
-
 static void app_log(const char *message, void *opaque)
 {
 	printf("%s\n", message);
@@ -68,10 +47,18 @@ int main(int argc, char **argv)
 	MTY_Frame frame = MTY_MakeDefaultFrame(0, 0, 1000, 700, 0.75);
 	MTY_WindowSetFrame(ctx.app, ctx.window, &frame);
 
-	ctx.webview = MTY_WindowCreateWebview(ctx.app, ctx.window);
-	MTY_WebviewAutomaticSize(ctx.webview, true);
-	MTY_WebviewMapVirtualHost(ctx.webview, "mty", "app.local", NULL);
-	MTY_WebviewShow(ctx.webview, on_webview_created);
+	const char *html =
+		"<html>"
+		"  <head>"
+		"    <style> body { background-color: darkslategrey; } </style>"
+		"  </head>"
+		"  <body>"
+		"    <span id='my_id'>Hello World!</span>"
+		"    <script> document.getElementById('my_id').style.color = 'goldenrod'; </script>"
+		"  </body>"
+		"</html>";
+
+	MTY_WebviewCreate(ctx.app, ctx.window, html, true);
 
 	MTY_AppRun(ctx.app);
 	MTY_AppDestroy(&ctx.app);

2ec81d999f12e888be5671a45ae52ead93c71231
Daniel Vijayakumar <daniel.vijayakumar@unity3d.com>
2024-09-12 21:02:46 -0400

    Added note for linux that channelsMask is insufficient to define channel config.

--- src/unix/linux/x11/audio.c
+++ src/unix/linux/x11/audio.c
@@ -58,6 +58,11 @@ MTY_Audio *MTY_AudioCreate(const MTY_AudioFormat *format, uint32_t minBuffer,
 	snd_pcm_hw_params_set_access(ctx->pcm, params, SND_PCM_ACCESS_RW_INTERLEAVED);
 	snd_pcm_hw_params_set_format(ctx->pcm, params, format->sampleFormat == MTY_AUDIO_SAMPLE_FORMAT_FLOAT ? SND_PCM_FORMAT_FLOAT : SND_PCM_FORMAT_S16);
 	snd_pcm_hw_params_set_channels(ctx->pcm, params, format->channels);
+	// XXX: Channel config for ALSA can't be specified via the opaque `format->channelsMask`
+	// Instead, an explicit channel mapping array is required. To be implemented in the future.
+	// See `snd_pcm_set_chmap`:
+	// 1. https://www.alsa-project.org/alsa-doc/alsa-lib/group___p_c_m.html#ga60ee7d2c2555e21dbc844a1b73839085
+	// 2. https://gist.github.com/raydudu/5590a196b9446c709c58a03eff1f38bc
 	snd_pcm_hw_params_set_rate(ctx->pcm, params, format->sampleRate, 0);
 	snd_pcm_hw_params(ctx->pcm, params);
 	snd_pcm_nonblock(ctx->pcm, 1);

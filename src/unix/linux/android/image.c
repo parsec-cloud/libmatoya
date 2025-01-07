01030f5cb9cd1eec396f2fe19faf8c5a93cf8683
Julien Loir <6706489+Namaneo@users.noreply.github.com>
2025-01-07 15:44:59 +0100

    Bugfix - [Android] Change overload of ImageDecoder.createSource

--- src/unix/linux/android/image.c
+++ src/unix/linux/android/image.c
@@ -22,8 +22,13 @@ void *MTY_DecompressImage(const void *input, size_t size, uint32_t *width, uint3
 
 	// Compressed input
 	jbyteArray jinput = mty_jni_dup(env, input, size);
+	jobject buffer = mty_jni_static_obj(env, "java/nio/ByteBuffer", "wrap",
+		"([B)Ljava/nio/ByteBuffer;", jinput);
+	if (!mty_jni_ok(env))
+		goto except;
+
 	jobject source = mty_jni_static_obj(env, "android/graphics/ImageDecoder", "createSource",
-		"([B)Landroid/graphics/ImageDecoder$Source;", jinput);
+		"(Ljava/nio/ByteBuffer;)Landroid/graphics/ImageDecoder$Source;", buffer);
 	if (!mty_jni_ok(env))
 		goto except;
 

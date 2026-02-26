d851dadbcb3a6b4b502b56abd7b5ac613eaad674
Martin Trang <martin.trang@unity3d.com>
2026-02-26 09:01:28 -0700

    fix typo

--- src/unix/apple/macosx/app.m
+++ src/unix/apple/macosx/app.m
@@ -766,7 +766,7 @@ static void window_scroll_event(struct window *ctx, NSEvent *event)
 	MTY_Event evt = {
 		.type = MTY_EVENT_SCROLL,
 		.window = ctx->window,
-		// macOS and Windows treat horizontal scrolling diffently,
+		// macOS and Windows treat horizontal scrolling differently,
 		// so invert the horizontal delta to match Windows behavior.
 		.scroll.x = lrint(-event.scrollingDeltaX * delta),
 		.scroll.y = lrint(event.scrollingDeltaY * delta),

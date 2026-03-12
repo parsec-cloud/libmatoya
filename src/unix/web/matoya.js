29a7c82d1bc9236eaf73ed0927fa7e24a82e5016
Martin Trang <martin.trang@unity3d.com>
2026-03-12 15:32:12 -0600

    Cleanup canvas

--- src/unix/web/matoya.js
+++ src/unix/web/matoya.js
@@ -665,7 +665,7 @@ async function mty_decode_image(input) {
 	const height = img.naturalHeight;
 
 	const canvas = new OffscreenCanvas(width, height);
-	const ctx = canvas.getContext('2d', {"willReadFrequently": true});
+	const ctx = canvas.getContext('2d');
 	ctx.drawImage(img, 0, 0, width, height);
 
 	return ctx.getImageData(0, 0, width, height);

5d6d748aeb2981c3dc78bac9e41ceea9739c8fd4
Martin Trang <martin.trang@unity3d.com>
2026-06-02 12:22:43 -0600

    Revert "Only return OS details"
    This reverts commit 8c543ecb9c056ecd0ece3911fdf203a56b672b1a.

--- src/unix/web/system.c
+++ src/unix/web/system.c
@@ -74,7 +74,9 @@ uint32_t MTY_GetPlatformNoWeb(void)
 MTY_OSInfo MTY_GetPlatformOSInfo(void)
 {
 	MTY_OSInfo info = {
-		.os = MTY_GetPlatformNoWeb(),
+		.os = MTY_OS_WEB,
+		.name = MTY_GetPlatformString(MTY_GetPlatformNoWeb()),
+		.valid_mask.name = true,
 	};
 	
 	return info;
